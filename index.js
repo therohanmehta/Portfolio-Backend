c440fe2d3a0179845cdd8866d70c206412bd7e19
therohanmehta <rohandhn786@gmail.com>
2023-07-16 00:06:30 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,51 @@
+const express = require("express");
+
+const cors = require("cors");
+
+const mongoose = require("mongoose");
+
+const db =
+  "mongodb+srv://rohandhn786:sBhGxLRPT97Rz7R2@portfoliodata.gbt9wgc.mongodb.net/portfoliodata";
+
+main().catch((err) => console.log(err));
+
+async function main() {
+  await mongoose.connect(db);
+  console.log("database connected");
+}
+
+const feedbackSchema = new mongoose.Schema({
+  name: String,
+  message: String,
+  time: String,
+  mac: String,
+  ip: String,
+});
+
+const FeedbackData = mongoose.model("feedbackData", feedbackSchema);
+
+const server = express();
+
+server.use(express.urlencoded({ extended: true }));
+
+server.use(express.json());
+
+server.use(cors());
+
+server.get("/data", async (req, res) => {});
+
+server.post("/data", async (req, res) => {
+  const feedbackData = new FeedbackData();
+  feedbackData.name = req.body.name;
+  feedbackData.time = req.body.time;
+  feedbackData.message = req.body.message;
+  feedbackData.mac = req.body.mac;
+  feedbackData.ip = req.body.ip;
+
+  const sendData = await feedbackData.save();
+  console.log(sendData);
+});
+
+server.listen(8080, () => {
+  console.log("server running");
+});
